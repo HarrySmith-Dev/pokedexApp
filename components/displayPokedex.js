439318de3f2e07301991a972701996d45063f248
HarrySmith-Dev <harry.smith0206@gmail.com>
2022-12-22 14:05:58 +0000

    sprites display fix

--- components/displayPokedex.js
+++ components/displayPokedex.js
@@ -3,28 +3,35 @@ import React, { useState, useEffect } from "react";
 import { View, Text, Image, FlatList, SafeAreaView } from "react-native";
 
 const DisplayPokedex = () => {
-  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
-    async function fetchPokemonData() {
-      await axios
-        .get("https://pokeapi.co/api/v2/pokemon")
+    for (let i = 1; i <= 50; i++) {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
         .then((response) => {
-          setPokemonData(response.data.results);
+          // The name can be found in response.data.name
+          // The sprite can be found in response.data.sprites.front_default
+          const name = response.data.name;
+          const sprite = response.data.sprites.front_default;
+          setPokemon((prevPokemon) => [...prevPokemon, { name, sprite }]);
         })
-        .catch((erorr) => {
-          console.warn(erorr);
+        .catch((error) => {
+          console.error(error);
         });
     }
-    fetchPokemonData();
   }, []);
 
   return (
     <FlatList
-      data={pokemonData}
+      data={pokemon}
       renderItem={({ item }) => (
         <SafeAreaView>
           <Text>{item.name}</Text>
+          <Image
+            source={{ uri: item.sprite }}
+            style={{ width: 100, height: 100 }}
+          />
         </SafeAreaView>
       )}
       keyExtractor={(item) => item.name}
