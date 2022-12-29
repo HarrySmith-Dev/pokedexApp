ffd63e1bc17a75bb7cb806f1eedba3ec43b33f1a
HarrySmith-Dev <harry.smith0206@gmail.com>
2022-12-29 17:34:05 +0000

    welcome screen v1 finished

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -17,19 +17,36 @@ const WelcomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.row}>
-        <View
-          style={[
-            styles.circles,
-            {
-              backgroundColor: colors.waterBlue,
-              borderColor: colors.white,
-              marginRight: 10,
-              padding: 30,
-              borderRadius: 35,
-              borderWidth: 5,
-            },
-          ]}
-        />
+        <View style={styles.outerCircle}>
+          <View
+            style={[
+              {
+                backgroundColor: colors.waterBlue,
+                width: 70,
+                height: 70,
+                borderRadius: 35,
+                borderWidth: 2,
+                shadowColor: "black",
+                shadowOffset: { width: 0, height: 1 },
+                shadowOpacity: 0.8,
+                shadowRadius: 2,
+              },
+            ]}
+          >
+            <View
+              style={[
+                styles.innerCircle,
+                {
+                  width: 20,
+                  height: 20,
+                  marginLeft: 10,
+                  marginTop: 10,
+                  borderRadius: 10,
+                },
+              ]}
+            />
+          </View>
+        </View>
         <View
           style={[
             styles.circles,
@@ -37,7 +54,9 @@ const WelcomeScreen = ({ navigation }) => {
               backgroundColor: colors.red,
             },
           ]}
-        />
+        >
+          <View style={styles.innerCircle} />
+        </View>
         <View
           style={[
             styles.circles,
@@ -45,14 +64,23 @@ const WelcomeScreen = ({ navigation }) => {
               backgroundColor: colors.goldenYellow,
             },
           ]}
-        />
-        <View style={styles.circles} />
+        >
+          <View style={styles.innerCircle} />
+        </View>
+        <View style={styles.circles}>
+          <View style={styles.innerCircle} />
+        </View>
       </View>
       <View style={styles.imageContainer}>
         <Image source={require("./img/pokedexImg.png")}></Image>
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button} onPress={() => OnPress()} />
+        <View style={styles.triangle} />
+      </View>
+
+      <View style={styles.barContainer}>
+        <View style={styles.bar} />
       </View>
     </SafeAreaView>
   );
@@ -71,10 +99,29 @@ const styles = StyleSheet.create({
   circles: {
     backgroundColor: colors.leafGreen,
     borderRadius: 10,
-    width: 20,
-    height: 20,
+    width: 15,
+    height: 15,
     borderWidth: 2,
-    marginLeft: 10,
+    marginLeft: 12,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+  },
+  outerCircle: {
+    backgroundColor: colors.white,
+    padding: 10,
+    borderRadius: 50,
+    borderWidth: 2,
+    marginRight: 20,
+  },
+  innerCircle: {
+    width: 5,
+    height: 5,
+    borderRadius: 5,
+    backgroundColor: colors.white,
+    marginTop: 1.5,
+    marginLeft: 0.5,
   },
   imageContainer: {
     flex: 1,
@@ -86,11 +133,37 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    flexDirection: "row-reverse",
   },
   button: {
     backgroundColor: colors.bostonRed,
     padding: 60,
     borderRadius: 60,
+    marginRight: 60,
+  },
+  triangle: {
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    borderTopWidth: 25, // Set the top border width to half the width of the triangle
+    borderRightWidth: 25, // Set the right and left border widths to the same value as the top border width
+    borderLeftWidth: 25,
+    borderTopColor: colors.electricYellow,
+    borderRightColor: "transparent",
+    borderLeftColor: "transparent",
+    transform: [{ rotate: "270deg" }], // Rotate the triangle by 45 degrees
+    marginRight: 10,
+  },
+  barContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bar: {
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 5,
+    paddingLeft: 80,
+    paddingRight: 80,
   },
 });
 
